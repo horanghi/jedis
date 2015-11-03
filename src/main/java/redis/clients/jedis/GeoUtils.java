39545ef1093a5822c084d5c2f13fd12eab2998c5
horanghi <juddcho@sk.com>
2015-11-03 17:31:34 +0900

    #SPATIAL-211 add proj4j mehtods

--- src/main/java/redis/clients/jedis/GeoUtils.java
+++ src/main/java/redis/clients/jedis/GeoUtils.java
@@ -23,7 +23,7 @@ public final class GeoUtils {
 	final static CoordinateTransform transWGS2Bessel = ctf.createTransform(wgs84, bessel1841);
 
 	@SuppressWarnings("rawtypes")
-	public static Point transBessl1841ToWGS84(final Point point) {
+	public static Point transBessel1841ToWGS84(final Point point) {
 
 		ProjCoordinate src = new ProjCoordinate(point.getX(), point.getY());
 		ProjCoordinate dest = new ProjCoordinate();
@@ -33,7 +33,7 @@ public final class GeoUtils {
 	}
 
 	@SuppressWarnings("rawtypes")
-	public static Point transWGS84ToBessl1841(final Point point) {
+	public static Point transWGS84ToBessel1841(final Point point) {
 
 		ProjCoordinate src = new ProjCoordinate(point.getX(), point.getY());
 		ProjCoordinate dest = new ProjCoordinate();
@@ -43,7 +43,7 @@ public final class GeoUtils {
 	}
 
 	@SuppressWarnings("rawtypes")
-	public static Point transBessl1841ToWGS84(double x, double y) {
+	public static Point transBessel1841ToWGS84(double x, double y) {
 
 		ProjCoordinate src = new ProjCoordinate(x, y);
 		ProjCoordinate dest = new ProjCoordinate();
@@ -53,7 +53,7 @@ public final class GeoUtils {
 	}
 
 	@SuppressWarnings("rawtypes")
-	public static Point transWGS84ToBessl1841(double x, double y) {
+	public static Point transWGS84ToBessel1841(double x, double y) {
 
 		ProjCoordinate src = new ProjCoordinate(x, y);
 		ProjCoordinate dest = new ProjCoordinate();
